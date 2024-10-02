ce679b0a00735d2a86ca5b491332d9b8dfda7330
kanchichauhan <k.chauhan27199@gmail.com>
2024-10-02 15:53:11 +0530

    HTTP methods added

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use('/hello', (req, res) => {
-    res.send('hello hello hello!')
-})
-app.use('/test', (req, res) => {
-    res.send('NAMASTE');
+app.get('/test', (req, res) => {
+    res.send('TEST GET');
+});
+app.post('/test', (req, res) => {
+    res.send('TEST POST');
+});
+app.delete('/test', (req, res) => {
+    res.send('TEST DELETE');
 });
 
-app.listen(7777, () => {
-    console.log('successfully logged in to the port 7777')
+app.listen(8001, () => {
+    console.log('successfully logged in to the port 7776')
 });
